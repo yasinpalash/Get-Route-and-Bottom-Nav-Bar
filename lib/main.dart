759f8d24ee2cfb30470fa187b5bf261185835ea4
yasinpalash <yasinmiapolash@gmail.com>
2024-10-11 10:24:45 +0600

    page view Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:getroute_navbar/bindings/bindings.dart';
-import 'package:getroute_navbar/routes/app_routes.dart';
-import 'package:getroute_navbar/screen/nav_bar/main_bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,10 +13,156 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialRoute: AppRoute.getNavBarRoute(),
-      getPages: AppRoute.routes,
       initialBinding: MainBinding(),
       home: const MainBottomNavBar(),
+    );
+  }
+}
+
+class MainBottomNavBar extends StatefulWidget {
+  const MainBottomNavBar({super.key});
+
+  @override
+  State<MainBottomNavBar> createState() => _MainBottomNavBarState();
+}
+
+class _MainBottomNavBarState extends State<MainBottomNavBar> {
+  final PageController _pageController = PageController();
+  int _selectedIndex = 0;
+
+  final List<Widget> screens = const [
+    HomeScreen(),
+    Screen2(),
+    Screen3(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Carts'),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text('Home Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const DetailsScreen(),
+                  ),
+                );
+              },
+              child: const Text('Go to Details Page'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Screen Two'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ViewPart(),
+                  ),
+                );
+              },
+              child: const Text('Go to View Page'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  const Screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Screen Three'),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Details Screen'),
+      ),
+    );
+  }
+}
+
+class ViewPart extends StatelessWidget {
+  const ViewPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('View Screen'),
+      ),
     );
   }
 }
